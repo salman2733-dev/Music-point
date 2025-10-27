a7f25a6bbeffa35a66ffddecb4a8117a17a3f261
Salman <salman@example.com>
2025-10-27 15:02:34 -0700

    Fix image paths

--- src/app/components/WhyChoose.tsx
+++ src/app/components/WhyChoose.tsx
@@ -10,7 +10,7 @@ const whyChooseData = [
     content: (
       <div className="w-full h-full flex items-center justify-center">
         <img
-          src="/public/mike.avif"
+          src="/mike.avif"
           alt="Expert Instructors"
           className="rounded-1xl w-full h-90 object-cover shadow-lg"
         />
@@ -24,7 +24,7 @@ const whyChooseData = [
     content: (
       <div className="w-full h-full flex items-center justify-center">
         <img
-          src="/public/gitar.avif"
+          src="/gitar.avif"
           alt="Personalized Learning"
           className="rounded-1xl w-full h-90 object-cover shadow-lg"
         />
@@ -38,7 +38,7 @@ const whyChooseData = [
     content: (
       <div className="w-full h-full flex items-center justify-center">
         <img
-          src="/public/lalala.avif"
+          src="/lalala.avif"
           alt="Modern Facilities"
           className="rounded-1xl w-full h-90 object-cover shadow-lg"
         />
@@ -52,7 +52,7 @@ const whyChooseData = [
     content: (
       <div className="w-full h-full flex items-center justify-center">
         <img
-          src="/public/you.avif"
+          src="/you.avif"
           alt="Performance Opportunities"
           className="rounded-1xl w-full h-90 object-cover shadow-lg "
         />
